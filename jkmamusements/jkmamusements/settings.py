5c65a2b8b1b9dc353263bdb94466ef632ee3a300
JKMamusements <jkmamusements@gmail.com>
2024-04-12 15:15:04 +0530

    checking statics

--- jkmamusements/jkmamusements/settings.py
+++ jkmamusements/jkmamusements/settings.py
@@ -132,6 +132,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
