a4d4729036a3f2d47871982d94f2330e7f3be9e5
JKMamusements <jkmamusements@gmail.com>
2024-04-12 14:07:43 +0530

    checking static

--- jkmamusements/jkmapp/urls.py
+++ jkmamusements/jkmapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='Home')
+    path('', views.home, name='Home'),
+    
 ]
--- jkmamusements/jkmapp/views.py
+++ jkmamusements/jkmapp/views.py
@@ -7,3 +7,6 @@ def members(request):
     return HttpResponse("Hello world!")
 # Create your views here.
 
+def home(request):
+    info = {}
+    return render(request, "home.html", info)
