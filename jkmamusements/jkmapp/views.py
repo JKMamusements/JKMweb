68a6c749f329b235e6b9012c7e6af73e457b0761
JKMamusements <jkmamusements@gmail.com>
2024-04-13 15:03:32 +0530

    testing again

--- jkmamusements/jkmapp/views.py
+++ jkmamusements/jkmapp/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def members(request):
-    return HttpResponse("Hello world!")
-# Create your views here.
-
 def home(request):
     info = {}
-    return render(request, "home.html", info)
+    return render(request, "index.html", info)
