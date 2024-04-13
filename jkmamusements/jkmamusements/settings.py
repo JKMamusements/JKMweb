26549423be0a1894ebe1c99b2a502a78e0bcf03f
JKMamusements <jkmamusements@gmail.com>
2024-04-13 11:22:05 +0530

    static

--- jkmamusements/jkmamusements/settings.py
+++ jkmamusements/jkmamusements/settings.py
@@ -34,8 +34,6 @@ CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
-
-
 SECURE_SSL_REDIRECT = \
     os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
 if SECURE_SSL_REDIRECT:
@@ -67,8 +65,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'jkmamusements.urls'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL= '/media/'
+
 
 
 TEMPLATES = [
@@ -137,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
