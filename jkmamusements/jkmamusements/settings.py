9d83e43d3b4a3a913771134c61df9211b0fa149f
JKMamusements <jkmamusements@gmail.com>
2024-04-13 11:31:27 +0530

    Update settings.py

--- jkmamusements/jkmamusements/settings.py
+++ jkmamusements/jkmamusements/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'jkmamusements.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
 
     }
 }
@@ -133,8 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
